dd10e8ead39f3d4efcda878f17b737725a2fd94c
Jidnyasa Bhandarkar <jidnayasa21@gmail.com>
2026-09-17 15:43:54 +0530

    calculator

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 
-print("\nHello,my name is", name)
+print("\nHello, my name is", name)
 print("I am", age, "years old.")
 print("Welcome to my Python learning journey! 🚀")
